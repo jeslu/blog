3d20289859848828dbc410e36470bb1bb8e6f72a
jeslu <perezangelesjesus@yahoo.com>
2021-06-14 00:45:09 -0500

    link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :clientes
-  get 'inicio/index'# optiene la ruta de inicio
+  #get 'inicio/index'# optiene la ruta de inicio
   resources :articles do
   resources :comments 
   end
