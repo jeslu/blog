302103bad59e177fa5ea2dc4af3a66cf6a100e8c
jeslu <perezangelesjesus@yahoo.com>
2021-06-14 00:57:58 -0500

    link cambio

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :clientes
-  #get 'inicio/index'# optiene la ruta de inicio
+  get 'inicio/index'# optiene la ruta de inicio
   resources :articles do
   resources :comments 
   end
 
 
-  root 'inicio#index'# optener la ruta de inicio en raiz 
+  #root 'inicio#index'# optener la ruta de inicio en raiz 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
